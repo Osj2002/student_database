e031377fb746564c7a960e7d6e1143ab0d1c41c8
Osj2002 <155349999+Osj2002@users.noreply.github.com>
2025-09-10 21:38:49 +0530

    Update Insert.java

--- Insert.java
+++ Insert.java
@@ -1,25 +1,49 @@
-
 import java.sql.*;
+import java.util.Scanner;
 
 public class Insert {
-    public static void main(String[] args){
-        //credentials to make the connection
+    public static void main(String[] args) {
+        // credentials to make the connection
         String url = "jdbc:mysql://localhost:3306/student_database";
         String user = "root";
-        String password = "DB_PASSWORD";
+        String password = "0987@Snowy";
 
-        try(Connection con = DriverManager.getConnection(url, user, password);
-            Statement stmt = con.createStatement()){
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement stmt = con.createStatement();
+             Scanner sc = new Scanner(System.in)) {
 
-            // input data to the student_database table
-            String sql = "INSERT INTO students (name, GPA,Department,age,year) VALUES "
-                    + "('Jone', 3.1,'Chemistry',22,1), "
-                    + "('Alice', 2.9,'Maths',23,4), "
-                    + "('Bob', 3.8,'Statistics',21,3)";
-            int rows= stmt.executeUpdate(sql);
+            System.out.println("No of students:");
+            int n = sc.nextInt();
+            sc.nextLine();
 
-            System.out.println("Records inserted");
-        }catch(Exception e){
+            for (int i = 1; i <= n; i++) {
+                System.out.println("Student " + i + ":");
+                System.out.print("Enter Name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Enter GPA: ");
+                double gpa = sc.nextDouble();
+                sc.nextLine();
+
+                System.out.print("Enter Department: ");
+                String dept = sc.nextLine();
+
+                System.out.print("Enter Age: ");
+                int age = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("ENter Year: ");
+                int year = sc.nextInt();
+                sc.nextLine();
+
+                String sql = "INSERT INTO students (name, GPA, Department, age, year) VALUES ('"
+                        + name + "', " + gpa + ", '" + dept + "', " + age + ", " + year + ")";
+
+                int rows = stmt.executeUpdate(sql);
+                System.out.println(" Records inserted.");
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
